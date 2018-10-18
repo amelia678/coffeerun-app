2d3079445a5acd6985f4d4e17e9bc56f47b97d42
Amelia Schulz email = amelia.schulz@gmail.com <ameliaschulz@Amelias-MacBook-Pro.local>
2018-10-18 15:02:26 -0400

    send post request with coffee form data

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,43 @@
 // ==================
 // DOM selection
 // ==================
-
+const orderForm = document.querySelector('[data-form]');
 
 // ==================
 // Helper functions
 // ==================
+function handleSubmit(event) {
+    event.preventDefault();
+    console.log('You get a coffee!')
+    console.log(event.target);
 
+    // we're gonna AJax tha thang.
+    // Call fetch()
+    // pass it the URL
+    // and an object with a method and a body
+    const url = event.target.action;
+    const method = event.target.method;
+    const elements = event.target.elements;
+    const data = {
+        strength: elements.strength.value,
+        flavor: elements.flavor.value,
+        size: elements.size.value,
+        coffee: elements.coffee.value,
+        emailAddress: elements.emailAddress.value,
+        
+    }
+    fetch(url, {
+        method: method,
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+            // "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: JSON.stringify(data)
+    })
+}
 
 // ==================
 // Main event listeners
 // ==================
+console.log('about to add event listenter!')
+orderForm.addEventListener('submit', handleSubmit);
