8ab4366a8458b70c6d364060644bc1deadc58526
Amelia Schulz email = amelia.schulz@gmail.com <ameliaschulz@Amelias-MacBook-Pro.local>
2018-10-19 11:32:05 -0400

    adds error notification

--- scripts/index.js
+++ scripts/index.js
@@ -2,19 +2,39 @@
 // DOM selection
 // ==================
 const orderForm = document.querySelector('[data-form]');
+const notificationArea = document.querySelector('[data-notification]');
+
+
+
 
 // ==================
 // Helper functions
 // ==================
+
+function notifyUser(notificationText) {
+    // create a div
+    const notificationBox = document.createElement('div')
+    
+    // add some text content
+    notificationBox.textContent = notificationText;
+    // must check for the exisitence of a .firstChild
+    
+    notificationArea.innerHTML = "";
+
+    // if (notificationArea.firstChild)
+    // notificationArea.removeChild(notificationArea.firstChild);
+    // append to...something .... somewhere ...???
+    notificationArea.appendChild(notificationBox);
+}
 function handleSubmit(event) {
     event.preventDefault();
     console.log('You get a coffee!')
     console.log(event.target);
 
-    // we're gonna AJax tha thang.
-    // Call fetch()
-    // pass it the URL
-    // and an object with a method and a body
+// we're gonna AJax tha thang.
+// Call fetch()
+// pass it the URL
+// and an object with a method and a body
     const url = event.target.action;
     const method = event.target.method;
     const elements = event.target.elements;
@@ -34,7 +54,17 @@ function handleSubmit(event) {
         },
         body: JSON.stringify(data)
     }).then(r => r.json())
-      .then(console.log)
+      .then( (orderInfo) => {
+        //   check the order info for errors
+            if (orderInfo.name === "ValidationError") {
+                notifyUser(`I'm sorry. Please fill out the coffee AND email address field. Thx`);
+            } else {
+                notifyUser(`Your coffee is being prepared by gnomes`);
+                
+            }
+            
+          //gotta wrap it in an anonymous function
+      })
 
 }
 
